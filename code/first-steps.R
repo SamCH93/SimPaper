8468ab811cc791119599182c4145e3dac64db905
lkook <lucasheinrich.kook@uzh.ch>
2021-07-27 17:18:29 +0200

    rough implementation of ai net

--- code/first-steps.R
+++ code/first-steps.R
@@ -1,6 +1,10 @@
+
+# Dependencies ------------------------------------------------------------
+
 library(randomForest)
 library(glmnet)
 
+# iris example ------------------------------------------------------------
 
 data("iris")
 
@@ -18,3 +22,79 @@ Y <- as.numeric(iris$bin) - 1
 
 plot(glmnet(x = X, y = Y, penalty.factor = pen.f, alpha = 0.5))
 plot(glmnet(x = X, y = Y, alpha = 0.5))
+
+# FUNs --------------------------------------------------------------------
+
+.rm_int <- function(X) {
+	if (all(X[, 1] == 1))
+		return(X[, -1, drop = FALSE])
+	return(X)
+}
+
+.importance_penalty <- function(rf, gamma = 1) {
+	imp <- rf$importance[, "MeanDecreaseGini"]
+	imp <- pmax(imp, 0)
+	1 - (imp / sum(imp))^gamma
+}
+
+.preproc <- function(formula, data) {
+	X <- .rm_int(model.matrix(formula, data))
+	Y <- model.response(model.frame(formula, data))
+	if (is.factor(Y) & length(levels(Y)) == 2L) {
+		Y <- as.numeric(Y) - 1
+	}
+	return(list(Y = Y, X = X))
+}
+
+fglmnet <- function(formula, data, ...) {
+	dat <- .preproc(formula, data)
+	glmnet(x = dat$X, y = dat$Y, ... = ...)
+}
+
+cv.fglmnet <- function(formula, data, ...) {
+	dat <- .preproc(formula, data)
+	rf <- randomForest(formula, data, importance = TRUE)
+	pen.f <- .importance_penalty(rf)
+	cv.glmnet(x = dat$X, y = dat$Y, penalty.factor = pen.f, ... = ...)
+}
+
+ai_net <- function(formula, data, plot = FALSE, ...) {
+	rf <- randomForest(formula, data, importance = TRUE)
+	if (plot)
+		varImpPlot(rf)
+	pen.f <- .importance_penalty(rf)
+	fglmnet(formula, data, penalty.factor = pen.f, ... = ...)
+}
+
+# Sim example -------------------------------------------------------------
+
+gen_dat <- function(n = 1e2, p = 10, b = rep(1:0, c(2, p -2))) {
+	X <- matrix(rnorm(n * p), nrow = n, ncol = p)
+	Y <- factor(rbinom(n = n, size = 1, plogis(X %*% b)))
+	return(data.frame(Y = Y, X = X))
+}
+
+res <- replicate(100, {
+	train <- gen_dat()
+	test <- gen_dat()
+	
+	fml <- Y ~ .
+	cvm <- cv.fglmnet(fml, train)
+	
+	m <- ai_net(fml, data = train, lambda = cvm$lambda.1se, plot = FALSE, alpha = 0.5)
+	
+	preds <- as.numeric(predict(m, newx = .rm_int(model.matrix(fml, test))) > 0.5)
+	
+	# bl <- glm(Y ~ ., data = train, family = binomial)
+	# pbl <- as.numeric(predict(bl, newdata = test, type = "response") > 0.5)
+	
+	dd <- .preproc(fml, train)
+	cbl <- cv.glmnet(x = dd$X, y = dd$Y, alpha = 0.5)
+	bl <- glmnet(x = dd$X, y = dd$Y, alpha = 0.5, lambda = cbl$lambda.1se)
+	pbl <- as.numeric(predict(bl, newx = .rm_int(model.matrix(fml, test))) > 0.5)
+	
+	c(AINET = mean(preds == test$Y),
+		BL = mean(pbl == test$Y))
+})
+
+boxplot(t(res))
