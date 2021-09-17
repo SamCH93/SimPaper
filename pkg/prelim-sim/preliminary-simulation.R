b0614c096113fc71b1f34ef8ad8f68a53f908829
SamCH93 <samuel.pawel@gmail.com>
2021-09-17 09:47:20 +0200

    Save options simDesign

--- pkg/prelim-sim/preliminary-simulation.R
+++ pkg/prelim-sim/preliminary-simulation.R
@@ -16,18 +16,32 @@ prev <- c(0.01, 0.05, 0.1)
 sigma2 <- c(1)
 rho <- c(0, 0.3, 0.6, 0.95)
 simGrid <- createDesign(n = n, EPV = EPV, prev = prev, sigma2 = sigma2,
-                       rho = rho, stringsAsFactors = FALSE)
+                        rho = rho, stringsAsFactors = FALSE)
 simGrid$p <- with(simGrid, ceiling(n * prev / EPV))
 nScenarios <- nrow(simGrid)
 
 # Simulation --------------------------------------------------------------
 
 res <- runSimulation(
-  simGrid[13, ],
-  replications = 10,
-  generate = generate,
-  analyse = analyze,
-  summarise = summarize,
-  save_seeds = TRUE,
-  fixed_objects = list(ntest = 1e4)
+    design = simGrid[13, ],
+    replications = 20, # TODO perform sample size calculation and adjust
+    generate = generate,
+    analyse = analyze,
+    summarise = NA, # TODO write summarize function to compute summary stats
+    save = TRUE,
+    save_seeds = TRUE,
+    save_results = TRUE,
+    save_details = list(safe = TRUE,
+                        save_results_dirname = "simResults",
+                        save_seeds_dirname = "simSeeds"),
+    parallel = TRUE,
+    ## ncores = 1000, # TODO set to cores of our simulation machine
+    fixed_objects = list(ntest = 1e4),
+    packages = c("ainet", "pROC", "glmnet")
 )
+
+
+
+## SimResults(results = res) ## can alternatively also read with readRDS
+## boxplot(brier ~ model,
+##         data = do.call("rbind", SimResults(results = res)$results))
