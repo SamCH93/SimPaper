88eb2d61083ca17fa63d43229876554280dd9d6b
lkook <lucasheinrich.kook@uzh.ch>
2021-11-09 16:36:22 +0100

    vis

--- simulation/vis-anova.R
+++ simulation/vis-anova.R
@@ -1,6 +1,6 @@
-# ANOVA for brier score
-# SP, LK, KR
-# Oct 2021
+# Vis anova
+# LK, SP, KR
+# Nov 2021
 
 # Deps --------------------------------------------------------------------
 
@@ -9,57 +9,11 @@ library(tidyverse)
 library(multcomp)
 library(ggpubr)
 
-outdir <- "results_anova"
-if (!dir.exists(outdir)) {
-    dir.create(outdir)
-}
+# Params ------------------------------------------------------------------
 
-# Load --------------------------------------------------------------------
+out <- "results_anova"
 
-files <- list.files(path = "simResults/")
-simres <- lapply(X = files, FUN = function(filename) {
-    resList <- readRDS(file = paste0("simResults/", filename))
-    lapply(X = resList$results, FUN = function(resListSimi) {
-        resListSimi$estimands
-    }) %>%
-        bind_rows()
-}) %>%
-    bind_rows()
-
-adat <- simres %>%
-    mutate(inputp = ceiling(n * prev / EPV)) %>%
-    filter(inputp != 1) %>%
-	mutate_at(c("n", "EPV", "prev", "rho"), ~ factor(.x, levels = sort(unique(.x)))) %>%
-	mutate(fct = factor(paste0(model, "n", n, "EPV", EPV, "prev", prev, "rho", rho)))
-
-# ANOVA -------------------------------------------------------------------
-
-run_anova <- function(formula = brier ~ 0 + fct, data = adat,
-                      conds = with(data, unique(paste0("n", n, "EPV", EPV, "prev", prev, "rho", rho))),
-                      models = c("GLM", "EN", "AEN", "RF"), compare_against = "AINET") {
-    m <- lm(formula, data = data)
-    out <- list()
-
-    pb <- txtProgressBar(min = 1, max = length(conds), style = 3)
-    for (cond in seq_along(conds)) {
-        setTxtProgressBar(pb, cond)
-        g1 <- paste0("fct", compare_against, conds[cond])
-        g2 <- paste0("fct", models, conds[cond])
-        lfct <- paste(g1, "-", g2, "== 0")
-        res <- try(glht(m, linfct = lfct))
-        if (inherits(res, "try-error"))
-            next
-        pval <- summary(res)$test$pvalues
-        cf <- confint(res)$confint
-        nms <- str_split(conds[cond], pattern = "[0-9]|\\.")[[1]]
-        nms <- nms[nms != ""]
-        nums <- str_split(conds[cond], pattern = "[a-z]|[A-Z]")[[1]]
-        nums <- nums[nums != ""]
-        names(nums) <- nms
-        out[[cond]] <- data.frame(cf, pval = pval, contrast = models, t(nums))
-    }
-    return(out)
-}
+# Funs --------------------------------------------------------------------
 
 vis_results <- function(pdat, xlab = "brier", save = TRUE) {
     
@@ -109,11 +63,6 @@ vis_results <- function(pdat, xlab = "brier", save = TRUE) {
 metrics <- c("brier", "scaledBrier", "nll", "acc", "auc")
 
 sapply(metrics, function(met) {
-    mdat <- adat %>% filter(is.finite(!!sym(met)))
-    cat("\nRemoved", nrow(adat) - nrow(mdat),
-        "rows due to infinite values / missingness in", met, "\n")
-    fml <- as.formula(paste(met, "~ 0 + fct"))
-    out <- run_anova(formula = fml, data = mdat)
     out <- read.csv(paste0("results_anova/anova_", met, ".csv"))
-    vis_results(out, xlab = met)
+    vis_results(out, xlab = met, save = FALSE)
 })
