140fbd3f223f8b96f9efd0391ebb42a19aec4ab0
lkook <lucasheinrich.kook@uzh.ch>
2022-02-09 13:29:05 +0100

    nop

--- simulation/anova-imputed.R
+++ simulation/anova-imputed.R
@@ -57,8 +57,7 @@ jdat <- full_join(adat, worst_cases, by = c("fct", "model"), suffix = c("", "_wc
 # ANOVA -------------------------------------------------------------------
 
 run_anova <- function(formula = brier ~ 0 + fct, data = adat,
-                      conds = with(data, unique(paste0("n", n, "EPV", EPV, "prev", prev, "rho", rho,
-                                                       "sparsity", sparsity))),
+                      conds = with(data, unique(paste0("n", n, "EPV", EPV, "prev", prev, "rho", rho))),
                       models = c("GLM", "EN", "AEN", "RF"), compare_against = "AINET") {
     m <- lm(formula, data = data)
     out <- list()
@@ -87,75 +86,13 @@ run_anova <- function(formula = brier ~ 0 + fct, data = adat,
 vis_results <- function(pdat, xlab = "brier", save = TRUE, 
                         lim = range(pdat$Estimate, na.rm = TRUE)) {
     
-    xxlab <- switch(xlab, "brier" = "Brier score",
-                    "scaledBrier" = "scaled Brier score",
-                    "nll" = "log score",
-                    "acc" = "accuracy",
-                    "auc" = "AUC")
-    
     out2 <- pdat %>% 
         bind_rows() %>% 
-        mutate_at(c("n", "EPV", "prev", "rho", "sparsity"), 
+        mutate_at(c("n", "EPV", "prev", "rho"), 
                   ~ factor(.x, levels = sort(unique(as.numeric(as.character(.x))))))
     
-    rho_plot <- function(trho, tsparse) {
-        ggplot(out2 %>% filter(rho == trho, sparsity == tsparse), 
-               aes(x = contrast, y = Estimate, ymin = lwr, ymax = upr,
-                   color = ordered(EPV))) +
-            geom_hline(yintercept = 0, linetype = 2, alpha = 0.5) +
-            geom_pointrange(fatten = 0.75, position = position_dodge(width = 0.7)) +
-            geom_errorbar(width = 0.35, position = position_dodge(width = 0.7)) +
-            facet_grid(prev ~ n, labeller = label_both) +
-            theme_bw() +
-            theme(legend.position = "top", panel.grid.major.y = element_blank(),
-                  axis.text.x = element_text(angle = 30, hjust = 1, vjust = 1, size = 7)) +
-            labs(y = paste("Difference in", xxlab, "(AINET - other method)"), 
-                 x = "Contrast", subtitle = bquote(rho==~.(trho)~sparsity==.(tsparse)), 
-                 color = "EPV") +
-            geom_vline(xintercept = seq(1.5, 3.5, 1), alpha = 0.1, size = 0.8) +
-            coord_flip()
-    }
-    
-    lapply(unique(as.numeric(as.character(out2$rho))), function(rho) {
-        ps <- lapply(unique(as.numeric(as.character(out2$sparsity))), function(sparse) {
-            rho_plot(trho = rho, tsparse = sparse)
-        })
-        pf <- ggarrange(plotlist = ps, common.legend = TRUE, ncol = 2, nrow = 2)
-        if (save) {
-            pnm <- file.path(outdir, paste0("tie-fighter_", xlab, "_rho", rho, ".pdf"))
-            ggsave(pnm, plot = pf, height = 1.5 * 8.3, width = 1.5 * 11.7)
-            write.csv(out2, file.path(outdir, paste0("anova_", xlab, ".csv")),
-                      row.names = FALSE, quote = FALSE)
-        }
-    })
-    
-    return(pf)
-}
-
-vis_na <- function(pdat, rhos = unique(as.numeric(as.character(pdat$rho))), 
-                   sparsities = unique(as.numeric(as.character(pdat$sparsity))),
-                   xlab = "brier") {
-    lapply(rhos, function(trho) {
-        lop <- lapply(sparsities, function(tsparse) {
-            pdat %>% 
-                filter(rho == trho, sparsity == tsparse) %>% 
-                ggplot(aes(x = model, y = EPV, fill = frac_na)) +
-                geom_tile(alpha = 0.3) +
-                geom_text(aes(label = paste0(frac_na, "%"))) +
-                facet_grid(prev ~ n, labeller = label_both) +
-                theme_bw() +
-                theme(panel.grid = element_blank()) +
-                labs(y = "EPV", 
-                     x = element_blank(), 
-                     subtitle = bquote(rho==~.(trho)~sparsity==.(tsparse)), 
-                     fill = paste0("Percent missing in ", xlab)) +
-                coord_flip() +
-                scale_fill_viridis_c()
-        })
-        pf <- ggarrange(plotlist = lop, common.legend = TRUE, ncol = 2, nrow = 2)
-        pnm <- file.path(outdir, paste0("missing_", xlab, "_rho", trho, ".pdf"))
-        ggsave(pnm, plot = pf, height = 1.5 * 8.3, width = 1.5 * 11.7)
-    })
+    write.csv(out2, file.path(outdir, paste0("anova_", xlab, ".csv")),
+              row.names = FALSE, quote = FALSE)
 }
 
 # Run ---------------------------------------------------------------------
@@ -173,5 +110,4 @@ sapply(metrics, function(met) {
     out <- run_anova(formula = fml, data = mdat)
     # out <- read.csv(file.path(outdir, paste0("anova_", met, ".csv")))
     try(vis_results(out, xlab = met))
-    try(vis_na(pdat = nadat, xlab = met))
 })
