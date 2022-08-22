a5e0cacb34119c6b52b0b9d689c0df35cc739325
SamCH93 <samuel.pawel@gmail.com>
2022-08-22 11:51:41 +0200

    polish the code for figure

--- code/figures-ainet.R
+++ code/figures-ainet.R
@@ -1,20 +1,19 @@
-# Figures for manuscript
-# LK, SP, KR
-# Feb 2022
+## Code to produce Figure 1 in "Pitfalls and Potentials in Simulation Studies"
+## Lucas Kook, Samuel Pawel, Kelly Reeve
+## August 2022
 
-# Deps --------------------------------------------------------------------
+## Dependencies ----------------------------------------------------------------
 
 library(tidyverse)
 library(ggpubr)
-library(ggbeeswarm)
-library(factoextra)
-library(patchwork)
-library(ggpp)
-theme_set(theme_bw())
+#library(ggbeeswarm)
+#library(factoextra)
+#library(patchwork)
+library(ggpp) # for position_dodgenudge
 
-# Params ------------------------------------------------------------------
+## Data ------------------------------------------------------------------------
 
-tsparse <- 0.3
+# names and labels of simulation studies
 tnms <- c(
     "prelim",
     "final",
@@ -32,73 +31,84 @@ tlabs <- c(
     "nonlinear"
 ) 
 
+# evaluation metrics
 which <- c("brier", "scaledBrier", "nll", "auc", "acc")
 
-tcol <- "gray60"
-
-folders <- c("preliminary-sim/results_anova/", 
-             "simulation/results_anova/",
-             "simulation/simResults-results-imputed/",
-             "hacking/simResults-nonlin_fix-results/",
-             "hacking/simResults-nonlin_fix-results-imputed/",
-             "hacking/simResults-nonlin-results/")
-
+# load data from simulation studies
+folders <- c("../preliminary-sim/results_anova/",
+             "../simulation/results_anova/",
+             "../simulation/simResults-results-imputed/",
+             "../hacking/simResults-nonlin_fix-results/",
+             "../hacking/simResults-nonlin_fix-results-imputed/",
+             "../hacking/simResults-nonlin-results/")
 paths <- expand_grid(folder = folders, metric = which)
 paths$path <- paste0(paths$folder, paste0("anova_", paths$metric, ".csv"))
-
 pdat <- paths %>% 
     mutate(d = map(path, ~ read_csv(.x, show_col_types = FALSE))) %>% 
     unnest(c(d)) %>% 
     mutate(set = factor(folder, levels = folders, labels = tlabs)) %>% 
     select(-path, -folder)
 
-## OG results
-pdatE46 <- pdat %>%
+## Create Figure 1 -------------------------------------------------------------
+
+# plot parameters
+palpha <- 0.9 # ? FIXME
+aalpha <- 0.7 # ? FIXME
+tsize <- 4 # ? FIXME
+ny <- 0 # ? FIXME
+tcol <- "gray60" # color for arrows ? FIXME
+plotDeco <- function(ggp) {
+    # function to add the same plot style to all plots
+    out <- ggp +
+        theme_bw() +
+        labs(x = "Difference in Brier score (negative: AINET better)",
+             y = element_blank(), color = "EPV") +
+        theme(panel.grid.major.y = element_blank(),
+              panel.grid.minor.x = element_blank()) +
+        geom_hline(yintercept = seq(1.5, 3.5, 1), alpha = 0.1, size = 0.8) +
+        scale_alpha_manual(values = c(0.1, 1)) +
+        scale_shape_manual(values = c("better" = 60, "worse" = 62))
+    return(out)
+}
+
+# subset of simulation conditions used for Figure
+corr <- 0.95 # correlation of covariates
+baseprev <- 0.05 # baseline prevalence
+spars <- 0.9 # proportion of regression coefficients which are non-zero (only
+             # for modified simulations)
+nmax <- 5000 # upper bound for sample size
+eval <- "brier" # evaluation metric
+pdatSubset <- pdat %>%
     filter(
-        rho == 0.95,
-        sparsity %in% c(0, NA),
-        set %in% c("final"), # , "final"),
-        # contrast != "EN",
-        metric == "brier",
-        n < 5000,
-        prev == 0.05,
-        # EPV < 10
+        rho == corr,
+        metric == eval,
+        n < nmax,
+        prev == baseprev
     )
 
-palpha <- 0.9
-aalpha <- 0.7
-tsize <- 4
-ny <- 0
-
-p0 <- ggplot(data = pdatE46, aes(y = contrast)) +
-    facet_grid(. ~ n, # scales = "free",
+## plot of pre-registered simulation results ("final")
+pdatOG <- pdatSubset %>%
+    filter(sparsity %in% c(0, NA), # no sparsity in pre-registered simulation
+           set %in% c("final"))
+p0raw <- ggplot(data = pdatOG, aes(y = contrast)) +
+    facet_grid(. ~ n,
                labeller = label_bquote(cols = italic(n) == .(n))) +
     geom_vline(xintercept = 0, lty = 2, alpha = 0.3) +
     geom_point(aes(x = Estimate, color = ordered(EPV)),
                position = position_dodge(width = 0.5), size = 0.8) +
     geom_errorbarh(aes(xmin = lwr, xmax = upr, color = ordered(EPV)),
                    position = position_dodge(width = 0.5),
-                   height = 0.25, show.legend = FALSE) +
-    labs(x = "Difference in Brier score (negative: AINET better)",
-         y = element_blank(), color = "EPV") +
-    theme(panel.grid.major.y = element_blank(),
-          panel.grid.minor.x = element_blank()) +
-    geom_hline(yintercept = seq(1.5, 3.5, 1), alpha = 0.1, size = 0.8) +
-    scale_alpha_manual(values = c(0.1, 1))
+                   height = 0.25, show.legend = FALSE)
+p0 <- plotDeco(ggp = p0raw)
 
-## E1
-colnames(pdat)
-pdatE1 <- pdat %>%
-    filter(
-        rho == 0.95,
-        sparsity %in% c(0, 0.9),
-        set %in% c("final", "nonlinear"),
-        metric == "brier",
-        n < 5000,
-        prev == 0.05
-    )
-pdatE1wide <- pdatE1 %>%
-    mutate(sparsity = 0.9) %>%
+## plot simulation results after QRP E2 modifying the DGP:
+## adding nonlinear effect and sparsity to DGP ("nonlinear")
+pdatE2 <- pdatSubset %>%
+    filter(sparsity %in% c(0, 0.9),
+           set %in% c("final", "nonlinear"))
+pdatE2wide <- pdatE2 %>%
+    # creating variable to indicate whether modified DGP improved performance
+    mutate(sparsity = 0.9) %>% # FIXME is this necessary?
     select(contrast, n, EPV, prev, rho, sparsity, Estimate, lwr, upr, set) %>%
     pivot_wider(names_from = set, values_from = c(Estimate, lwr, upr)) %>%
     mutate(better = ifelse(Estimate_nonlinear < Estimate_final, "better", "worse"),
@@ -106,59 +116,42 @@ pdatE1wide <- pdatE1 %>%
            to = ifelse(better  == "better", upr_nonlinear, upr_final),
            head = ifelse(better == "better", upr_nonlinear, lwr_nonlinear),
            head = ifelse((pmin(lwr_nonlinear, lwr_final) <= pmax(upr_nonlinear, upr_final) &
-                    pmax(lwr_nonlinear, lwr_final) <= pmin(upr_nonlinear, upr_final)),
-               NA, head))
-
-# pdatE1 %>% filter(n == 1000, contrast == "RF")
-# pdatE1wide %>% filter(n == 1000, contrast == "RF")
-
-p1 <- ggplot(data = pdatE1, aes(y = contrast)) +
-    facet_grid(. ~ n, # scales = "free",
+                          pmax(lwr_nonlinear, lwr_final) <= pmin(upr_nonlinear, upr_final)),
+                         NA, head))
+p1raw <- ggplot(data = pdatE2, aes(y = contrast)) +
+    facet_grid(. ~ n,
                labeller = label_bquote(cols = italic(n) == .(n))) +
     geom_vline(xintercept = 0, lty = 2, alpha = 0.3) +
-    geom_linerange(data = pdatE1wide, alpha = aalpha, lwd = 0.3,
+    geom_linerange(data = pdatE2wide, alpha = aalpha, lwd = 0.3,
                    aes(xmin = from, xmax = to, y = contrast,
-                       color = NULL, group = ordered(EPV)), color = tcol, # ordered(EPV)),
+                       color = NULL, group = ordered(EPV)), color = tcol,
                    position = position_dodgenudge(0.5, y = ny),
                    show.legend = FALSE) +
-    geom_point(data = pdatE1wide,
+    geom_point(data = pdatE2wide,
                aes(x = head, group = ordered(EPV), shape = better),
                size = tsize, position = position_dodgenudge(width = 0.5, y = ny),
                alpha = aalpha, show.legend = FALSE, color = tcol) +
     geom_errorbarh(aes(xmin = lwr, xmax = upr, color = ordered(EPV)),
                    position = position_dodge(width = 0.5), alpha = palpha,
                    height = 0.25, show.legend = FALSE) +
-    ## geom_point(aes(x = Estimate, color = ordered(EPV)), size = 0.8,
-    ##            position = position_dodge(width = 0.5), alpha = 0.5) +
-    geom_point(data = pdatE1wide,
+    geom_point(data = pdatE2wide,
                aes(x = Estimate_final, color = ordered(EPV)), size = 0.8,
                position = position_dodge(width = 0.5), alpha = palpha) +
-    geom_point(data = pdatE1wide,
+    geom_point(data = pdatE2wide,
                aes(x = Estimate_nonlinear, color = ordered(EPV)), size = 0.8,
-               position = position_dodge(width = 0.5), alpha = palpha) +
-    scale_shape_manual(values = c("better" = 60, "worse" = 62)) +
-    labs(x = "Difference in Brier score (negative: AINET better)",
-         y = element_blank(), color = "EPV") +
-    theme(panel.grid.major.y = element_blank(),
-          panel.grid.minor.x = element_blank()) +
-    geom_hline(yintercept = seq(1.5, 3.5, 1), alpha = 0.1, size = 0.8)
-#2" ggsave(p1, filename = "code/E1.pdf", height = 4.5)
+               position = position_dodge(width = 0.5), alpha = palpha)
+p1 <- plotDeco(ggp = p1raw)
 
-## E1 + E3
-pdatE46 <- pdat %>%
-    filter(
-        rho == 0.95,
-        sparsity %in% c(0, NA, 0.9),
-        set %in% c("nonlinear", "final"),
-        metric == "brier",
-        n < 5000,
-        prev == 0.05
-    ) %>% 
+## plot simulation results after QRPs E2 + E3 (removing competitor EN)
+pdatE3 <- pdatSubset %>%
+    filter(sparsity %in% c(0, NA, 0.9),
+           set %in% c("nonlinear", "final")) %>%
     mutate(alp1 = contrast != "EN", 
            alp = paste0(EPV, contrast != "EN"),
            alp = ordered(alp, levels = str_sort(unique(alp), numeric = TRUE)))
-pdatE46wide <- pdatE46 %>%
-    mutate(sparsity = 0.9) %>%
+pdatE3wide <- pdatE3 %>%
+    # creating variable to indicate whether modified DGP improved performance
+    mutate(sparsity = 0.9) %>% # FIXME is this necessary?
     select(contrast, n, EPV, prev, rho, sparsity, Estimate, lwr, upr, set, alp, alp1) %>%
     pivot_wider(names_from = set, values_from = c(Estimate, lwr, upr)) %>%
     mutate(better = ifelse(Estimate_nonlinear < Estimate_final, "better", "worse"),
@@ -168,74 +161,51 @@ pdatE46wide <- pdatE46 %>%
            head = ifelse((pmin(lwr_nonlinear, lwr_final) <= pmax(upr_nonlinear, upr_final) &
                     pmax(lwr_nonlinear, lwr_final) <= pmin(upr_nonlinear, upr_final)),
                NA, head))
-    # spread(key = set, value = Estimate) %>%
-    # mutate(better = ifelse(nonlinear < final, "better", "worse"))
-
-p2 <- ggplot(data = pdatE46, aes(y = contrast)) +
-    facet_grid(. ~ n, # scales = "free",
+p2raw <- ggplot(data = pdatE3, aes(y = contrast)) +
+    facet_grid(. ~ n,
                labeller = label_bquote(cols = italic(n) == .(n))) +
     geom_vline(xintercept = 0, lty = 2, alpha = 0.3) +
-    geom_linerange(data = pdatE46wide, lwd = 0.3, # alpha = aalpha,
+    geom_linerange(data = pdatE3wide, lwd = 0.3,
                    aes(xmin = from, xmax = to, y = contrast, alpha = alp,
                        color = NULL), color = tcol,
                    position = position_dodgenudge(0.5, y = ny),
                    show.legend = FALSE) +
-    geom_point(data = pdatE46wide,
+    geom_point(data = pdatE3wide,
                aes(x = head, color = NULL, alpha = alp, shape = better), 
-               size = tsize, position = position_dodgenudge(width = 0.5, y = ny), # alpha = aalpha,
+               size = tsize, position = position_dodgenudge(width = 0.5, y = ny),
                color = tcol, show.legend = FALSE) +
     geom_errorbarh(aes(xmin = lwr, xmax = upr, color = ordered(EPV), alpha = alp1),
-                   position = position_dodge(width = 0.5), # alpha = palpha,
+                   position = position_dodge(width = 0.5),
                    height = 0.25, show.legend = FALSE) +
-    ## geom_point(aes(x = Estimate, color = ordered(EPV)), size = 0.8,
-    ##            position = position_dodge(width = 0.5), alpha = 0.5) +
-    geom_point(data = pdatE46wide,
+    geom_point(data = pdatE3wide,
                aes(x = Estimate_final, color = ordered(EPV), alpha = alp1), size = 0.8,
-               position = position_dodge(width = 0.5)) + # , alpha = palpha) +
-    geom_point(data = pdatE46wide,
+               position = position_dodge(width = 0.5)) +
+    geom_point(data = pdatE3wide,
                aes(x = Estimate_nonlinear, color = ordered(EPV), alpha = alp1), size = 0.8,
-               position = position_dodge(width = 0.5)) + # , alpha = palpha) +
-    scale_shape_manual(values = c("better" = 60, "worse" = 62)) +
-    labs(x = "Difference in Brier score (negative: AINET better)",
-         y = element_blank(), color = "EPV") +
-    theme(panel.grid.major.y = element_blank(),
-          panel.grid.minor.x = element_blank()) +
+               position = position_dodge(width = 0.5))
+p2 <- plotDeco(ggp = p2raw) +
     scale_alpha_manual(values = c(rep(c(0.1, aalpha), 4), 0.1, palpha)) +
-    geom_hline(yintercept = seq(1.5, 3.5, 1), alpha = 0.1, size = 0.8) +
     theme(axis.text.y.left = element_text(color = c("gray30", "gray80", "gray30", "gray30")))
 
-## E1 + E3 + E6
-pdatE46 <- pdat %>%
-    filter(
-        rho == 0.95,
-        sparsity %in% c(0, NA, 0.9),
-        set %in% c("nonlinear"), # , "final"),
-        # contrast != "EN",
-        metric == "brier",
-        n < 5000,
-        prev == 0.05,
-        # EPV < 10
-    ) %>% 
+## plot simulation results after QRPs E2 + E3 + R2 (selective reporting of low EPV settings)
+pdatR2 <- pdatSubset %>%
+    filter(sparsity %in% c(0, NA, 0.9),
+           set %in% c("nonlinear")) %>%
     mutate(alp = contrast != "EN" & EPV < 10)
-
-p3 <- ggplot(data = pdatE46, aes(y = contrast, alpha = alp)) +
-    facet_grid(. ~ n, # scales = "free",
+p3raw <- ggplot(data = pdatR2, aes(y = contrast, alpha = alp)) +
+    facet_grid(. ~ n,
                labeller = label_bquote(cols = italic(n) == .(n))) +
     geom_vline(xintercept = 0, lty = 2, alpha = 0.3) +
     geom_point(aes(x = Estimate, color = ordered(EPV), alpha = alp),
                position = position_dodge(width = 0.5), size = 0.8) +
     geom_errorbarh(aes(xmin = lwr, xmax = upr, color = ordered(EPV)),
                    position = position_dodge(width = 0.5),
-                   height = 0.25, show.legend = FALSE) +
-    labs(x = "Difference in Brier score (negative: AINET better)",
-         y = element_blank(), color = "EPV") +
-    theme(panel.grid.major.y = element_blank(),
-          panel.grid.minor.x = element_blank()) +
-    geom_hline(yintercept = seq(1.5, 3.5, 1), alpha = 0.1, size = 0.8) +
-    scale_alpha_manual(values = c(0.1, 1)) +
+                   height = 0.25, show.legend = FALSE)
+p3 <- plotDeco(ggp = p3raw) +
     theme(axis.text.y.left = element_text(color = c("gray30", "gray80", "gray30", "gray30"))) +
     xlim(min(p1$data$Estimate) * 1.05, max(p1$data$Estimate) * 1.13)
 
+## combine plots in one Figure and save
 ggarrange(
     p0 + labs(subtitle = "Per-protocol"),
     p1 + labs(subtitle = "QRP: E2"),
@@ -243,6 +213,5 @@ ggarrange(
     p3 + labs(subtitle = "QRP: E2 + E3 + R2 (reported)"),
     common.legend = TRUE, ncol = 1, legend = "right"
 )
-
 ggsave("ainet-results.pdf", height = 13 * 0.9, width = 10 * 0.9)
 
