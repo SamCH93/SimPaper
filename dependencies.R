aee2d81c509d77c19f459e56504b8b4d60ca6adc
lkook <lucasheinrich.kook@uzh.ch>
2022-08-30 10:33:31 +0200

    mirror

--- dependencies.R
+++ dependencies.R
@@ -1,12 +1,12 @@
 
 if (!require(remotes))
-  install.packages("remotes")
+  install.packages("remotes", repo = "http://cran.ch.r-project.org")
 
 if (!require(tidyverse))
-  install.packages("tidyverse")
+  install.packages("tidyverse", repo = "http://cran.ch.r-project.org")
 
 if (!require(ggpp))
-  install.packages("ggpp")
+  install.packages("ggpp", repo = "http://cran.ch.r-project.org")
 
 remotes::install_github("LucasKook/ainet", ref = "0.0-1")
 
